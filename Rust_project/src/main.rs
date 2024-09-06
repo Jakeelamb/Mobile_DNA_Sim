0bf4ef6b4a5f8f8d9ee201e1e72fb61e24b5a950
lalaluke9 <ldowers@colostate.edu>
2024-09-05 23:06:32 -0500

    notes + setup

--- Rust_project/src/main.rs
+++ Rust_project/src/main.rs
@@ -1,3 +1,21 @@
+mod copy_and_paste;
+mod cut_and_paste;
+
+use copy_and_paste::CopyAndPaste;
+use cut_and_paste::CutAndPaste;
+
 fn main() {
-    println!("Hello, world!");
+    let cp = CopyAndPaste {
+        sequence: String::from("Copy and Paste"),
+    };
+
+    let result = cp.print();
+    println!("Result: {}", result);
+
+    let cut = CutAndPaste {
+        sequence: String::from("Cut and Paste"),
+    };
+
+    let result = cut.print();
+    println!("Result: {}", result);
 }
