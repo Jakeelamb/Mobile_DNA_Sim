3061a2a8207af233482e5aa06dcee4db9748448f
jake <jakelamb74@gmail.com>
2024-10-03 16:43:54 -0600

    fixed simulation

--- Jake/src/main.rs
+++ Jake/src/main.rs
@@ -4,6 +4,5 @@ use te_sim::run_simulation;
 fn main() -> Result<(), Box<dyn Error>> {
     let num_rounds = 1000; // or however many rounds you want to run
     let final_param = run_simulation(num_rounds)?;
-    final_param.write_results_to_file("simulation_results.csv")?;
     Ok(())
 }
