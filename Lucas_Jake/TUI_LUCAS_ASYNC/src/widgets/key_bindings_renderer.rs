3cd85100c9cc183015eb0aa20b1798fd61118fb7
lalaluke9 <ldowers@colostate.edu>
2024-11-06 21:20:37 -0600

    key bindings edit

--- Lucas_Jake/TUI_LUCAS_ASYNC/src/widgets/key_bindings_renderer.rs
+++ Lucas_Jake/TUI_LUCAS_ASYNC/src/widgets/key_bindings_renderer.rs
@@ -3,12 +3,11 @@ use ratatui::style::{Style, Color};
 
 pub fn render_key_bindings() -> Paragraph <'static> {
     let key_bindings_text = "
-    [Home Page]            | [q] Exit   | [Right] Simulation Page  | [Left] Previous theme
-    [Select Species]      | [Up Arrow] | [Down Arrow] [/] Search | [Esc] Exit search
-    [Simulation Settings] | [Tab] Select Sim settings   | [Left] Previous theme
-    [Start Simulation [Enter]\n
+    [Home Page]           | [q] Exit                             | [Right Arrow] Simulation Page  | [Left Arrow] Previous theme
+    [Select Species]      | [Up Arrow] Up                        | [Down Arrow] Down              | [/] Search  | [Esc] Exit search
+    [Simulation Settings] | [Tab] Scrolls through Sim settings   | [delete/backspace] deletes editable settings
 
-    [Simulation Page]     | [s] Start simulation   | [Left] Back to home to select new species
+    [Simulation Page]     | [s] Start simulation                 | [Left] Back to home to select new species
     ";
         Paragraph::new(key_bindings_text)
             .block(Block::default().borders(Borders::ALL).title("Tab list"))
