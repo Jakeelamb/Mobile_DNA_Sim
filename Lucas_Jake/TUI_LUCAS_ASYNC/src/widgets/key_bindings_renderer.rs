f0567be8c5ad696aab49cf4d75e37af14ee94199
lalaluke9 <ldowers@colostate.edu>
2024-11-06 21:28:33 -0600

    keybindings edit

--- Lucas_Jake/TUI_LUCAS_ASYNC/src/widgets/key_bindings_renderer.rs
+++ Lucas_Jake/TUI_LUCAS_ASYNC/src/widgets/key_bindings_renderer.rs
@@ -4,7 +4,7 @@ use ratatui::style::{Style, Color};
 pub fn render_key_bindings() -> Paragraph <'static> {
     let key_bindings_text = "
     [Home Page]           | [q] Exit                             | [Right Arrow] Simulation Page  | [Left Arrow] Previous theme
-    [Select Species]      | [Up Arrow] Up                        | [Down Arrow] Down              | [/] Search  | [Esc] Exit search
+    [Species List]      | [Up Arrow] Up                        | [Down Arrow] Down              | [/] Search  | [Esc] Exit search
     [Simulation Settings] | [Tab] Scrolls through Sim settings   | [delete/backspace] deletes editable settings
 
     [Simulation Page]     | [s] Start simulation                 | [Left] Back to home to select new species
